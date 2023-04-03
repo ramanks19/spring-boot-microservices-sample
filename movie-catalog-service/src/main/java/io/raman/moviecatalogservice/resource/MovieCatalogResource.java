f55d4fc094a20b009155009bff61e720875aeae6
ramanks19 <ramans.91@rediffmail.com>
2023-04-04 00:17:04 +0530

    Code for WebClient

--- movie-catalog-service/src/main/java/io/raman/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/raman/moviecatalogservice/resource/MovieCatalogResource.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import io.raman.moviecatalogservice.models.CatalogItem;
 import io.raman.moviecatalogservice.models.Rating;
@@ -19,11 +20,11 @@ import io.raman.moviecatalogservice.models.Movie;
 @RequestMapping("/catalog")
 public class MovieCatalogResource{
 
-    /*
-     * Autowired is a consumer and Bean is a producer.
-     */
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -34,7 +35,14 @@ public class MovieCatalogResource{
         );
 
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = webClientBuilder.build()
+                                          .get()
+                                          .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                                          .retrieve()
+                                          .bodyToMono(Movie.class)
+                                          .block();
+            
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         })
         .collect(Collectors.toList());
