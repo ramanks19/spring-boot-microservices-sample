96f19bccb92967eef63a543cbeec23bfeecfb9c9
ramanks19 <ramans.91@rediffmail.com>
2023-04-15 19:36:40 +0530

    Changes related to External API

--- movie-catalog-service/src/main/java/io/raman/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/raman/moviecatalogservice/resource/MovieCatalogResource.java
@@ -30,10 +30,10 @@ public class MovieCatalogResource{
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/user/" + userId, UserRating.class);
+        UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/" + userId, UserRating.class);
 
         return ratings.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
             return new CatalogItem(movie.getName(), "Test", rating.getRating());
         })
         .collect(Collectors.toList());
