cf23133945177d0e85a7d9a5b9d41e827cbb324c
ramanks19 <ramans.91@rediffmail.com>
2023-04-15 16:17:54 +0530

    Changes for Loadbalancer

--- movie-catalog-service/src/main/java/io/raman/moviecatalogservice/resource/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/raman/moviecatalogservice/resource/MovieCatalogResource.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
